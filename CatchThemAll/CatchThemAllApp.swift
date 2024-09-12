b9363dc38e9ac00b30140099a928c2424f820916
lorirothermel <lori@rothermel1.com>
2024-09-12 16:17:05 -0400

    Initial Commit

---
+++ CatchThemAll/CatchThemAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchThemAllApp.swift
+//  CatchThemAll
+//
+//  Created by Lori Rothermel on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CatchThemAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
